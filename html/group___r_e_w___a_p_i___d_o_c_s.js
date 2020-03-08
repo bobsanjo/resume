cad5f358ba9adb05c2d6d4feea935e3441578c18
bobsanjo <61819622+bobsanjo@users.noreply.github.com>
2020-03-08 13:25:23 -0700

    Add files via upload

---
+++ html/group___r_e_w___a_p_i___d_o_c_s.js
@@ -0,0 +1,6 @@
+var group___r_e_w___a_p_i___d_o_c_s =
+[
+    [ "Some Methods", "group__sdk__api3.html", null ],
+    [ "Service Methods", "group__sdk__api.html", null ],
+    [ "Other Methods", "group__sdk__api2.html", null ]
+];
---
+++ html/group___r_e_w___c_o_r_e___d_o_c_s.js
@@ -0,0 +1,11 @@
+var group___r_e_w___c_o_r_e___d_o_c_s =
+[
+    [ "About", "group__about.html", null ],
+    [ "Introduction to this Site", "group__intro.html", null ],
+    [ "Tutorial Guides", "group___t_u_t_o_r_i_a_l___g_u_i_d_e.html", null ],
+    [ "User Guides", "group___u_s_e_r___g_u_i_d_e.html", null ],
+    [ "Testing Guides", "group___t_e_s_t_i_n_g___g_u_i_d_e.html", null ],
+    [ "Quick Start Guides", "group___q_u_i_c_k___s_t_a_r_t___g_u_i_d_e.html", null ],
+    [ "Release Notes ver. 1.0", "group__release__notes.html", null ],
+    [ "Robert's Resume", "group__resume.html", null ]
+];
