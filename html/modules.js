50c1a9dc3ec77ec3c82e810fb271356736cfa778
bobsanjo <61819622+bobsanjo@users.noreply.github.com>
2020-03-08 13:56:26 -0700

    Add files via upload

---
+++ html/modules.js
@@ -0,0 +1,5 @@
+var modules =
+[
+    [ "API Docs", "group___r_e_w___a_p_i___d_o_c_s.html", "group___r_e_w___a_p_i___d_o_c_s" ],
+    [ "Core Documents", "group___r_e_w___c_o_r_e___d_o_c_s.html", "group___r_e_w___c_o_r_e___d_o_c_s" ]
+];
---
+++ html/navtreedata.js
@@ -0,0 +1,25 @@
+var NAVTREE =
+[
+  [ "API Documentation Example - R. Williams", "index.html", [
+    [ "about", "md_about.html", null ],
+    [ "ap3", "md_ap3.html", null ],
+    [ "api", "md_api.html", null ],
+    [ "api2", "md_api2.html", null ],
+    [ "introduction", "md_introduction.html", null ],
+    [ "release-notes", "md_release-notes.html", null ],
+    [ "resume", "md_resume.html", null ],
+    [ "tut1", "md_tut1.html", null ],
+    [ "tut2", "md_tut2.html", null ],
+    [ "user1", "md_user1.html", null ],
+    [ "user2", "md_user2.html", null ],
+    [ "Modules", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"group___q_u_i_c_k___s_t_a_r_t___g_u_i_d_e.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
