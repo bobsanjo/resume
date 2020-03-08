ffd26e2a6eccb800ba4b3125ad79154dc9bc86e2
bobsanjo <61819622+bobsanjo@users.noreply.github.com>
2020-03-08 14:42:32 -0700

    Add files via upload

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -1,19 +1,28 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
 var NAVTREE =
 [
   [ "API Documentation Example - R. Williams", "index.html", [
-    [ "about", "md_about.html", null ],
-    [ "ap3", "md_ap3.html", null ],
-    [ "api", "md_api.html", null ],
-    [ "api2", "md_api2.html", null ],
-    [ "introduction", "md_introduction.html", null ],
-    [ "qs1", "md_qs1.html", null ],
-    [ "qs2", "md_qs2.html", null ],
-    [ "release-notes", "md_release-notes.html", null ],
-    [ "resume", "md_resume.html", null ],
-    [ "tut1", "md_tut1.html", null ],
-    [ "tut2", "md_tut2.html", null ],
-    [ "user1", "md_user1.html", null ],
-    [ "user2", "md_user2.html", null ],
     [ "Modules", "modules.html", "modules" ]
   ] ]
 ];
